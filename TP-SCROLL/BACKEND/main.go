7e8db12af8f84a782b3c491627b0aa92a05e9cf8
Ikhsan Assidiqie <ikhsanir21@gmail.com>
2022-05-17 06:05:55 +0700

    main

--- TP-SCROLL/BACKEND/main.go
+++ TP-SCROLL/BACKEND/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import . "fmt"
+
+type CustomerData struct {
+	fullName, fullAddress, posCode, phoneNumber string
+	distributingOption                          int
+}
+
+// Struct just like a Class in Java
+
+func dynamicCallOut(name, address, pos, phone string, distributing int) CustomerData {
+	dynamicSortOf := CustomerData{
+		fullName:           name,
+		fullAddress:        address,
+		posCode:            pos,
+		phoneNumber:        phone,
+		distributingOption: distributing,
+	}
+
+	return dynamicSortOf
+}
 
 func main() {
-	fmt.Println("Main Go")
+	var (
+		name, address, pos, phone string
+		distributing, pickMenu, n int
+	)
+
+	Println("Welcome to 141's Armoury.")
+	Println("What do you want to be served today?")
+	Println("1. Submit Form \n2. Exit")
+
+	Scanln(&pickMenu)
+	Scanln(&n)
+
+	for i := 1; i <= n; i++ {
+		// LOOPING HOW MANY CLIENTS
+	}
+
+	if pickMenu == 1 {
+		Println("Please fill this form belo: ")
+		Println("Format:\nName, Address, Postal Code, Phone, Distributing Option")
+		Scanln(&name, &address, &pos, &phone, &distributing)
+	} else if pickMenu == 2 {
+
+	} else {
+		Println("Invalid input.")
+	}
+
+	Println(dynamicCallOut(name, address, pos, phone, distributing))
 }
