f6947789fd2f57f4d79363c142b485681bd379c8
Ikhsan Assidiqie <91542488+ikhsansdqq@users.noreply.github.com>
2022-05-17 15:08:54 +0700

    Create Student Problem

---
+++ TP-SCROLL/BACKEND/student.go
@@ -0,0 +1,28 @@
+package main
+
+import . "fmt"
+
+type Array struct {
+	gpa float64
+}
+
+func highest() {
+
+}
+
+func lowest() {
+
+}
+
+func avg() {
+
+}
+
+func main() {
+	Println("Max: ")
+}
+
+// FINDING HIGHEST EPRT SCORE
+// FINDING LOWEST GPA
+// AVERAGE ALL SEMESTER TAKEN
+// STUDENTS !=> 1.000 STUDENTS
