beb7503b96ee4213c6c75e187b29c3fb1020c904
Ikhsan Assidiqie <91542488+ikhsansdqq@users.noreply.github.com>
2022-05-18 08:56:13 +0700

    update students.go

--- TP-SCROLL/BACKEND/student.go
+++ TP-SCROLL/BACKEND/student.go
@@ -2,7 +2,9 @@ package main
 
 import . "fmt"
 
-type MyArrays struct {
+// TODO: import base.json into student.go
+
+type BaseArray struct {
 	name      string
 	eprt, gpa float64
 	sem       int
@@ -11,21 +13,41 @@ type MyArrays struct {
 // --> {name, gpa, eprt, sem} constructor
 
 func main() {
-	ikhsan := MyArrays{
-		name: "Ikhsan Assidiqie",
+
+	users := BaseArray{
+		name: "John Doe",
 		eprt: 5.4,
 		gpa:  2.8,
 		sem:  4,
-	}
+	} // PROJECTING ARRAYS INTO BaseArray
+	// PULLING FILES LOGIC
 
-	Println("Name:", ikhsan.name,
-		"\nGPA:", ikhsan.gpa,
-		"\nEPRT Score:", ikhsan.eprt,
-		"\nCurrent Semester:", ikhsan.sem)
+	Println("Name:", users.name,
+		"\nGPA:", users.gpa,
+		"\nEPRT Score:", users.eprt,
+		"\nCurrent Semester:", users.sem)
 }
 
-func highestEprt(customArray MyArrays) {
-	
+func highestEprt(N int) int {
+	i := 1
+
+	users := BaseArray{
+		name: "John Doe",
+		eprt: 5.4, // (array[index].eprt)
+		gpa:  2.8, // (array[index].gpa)
+		sem:  4,   // (array[index].sem)
+	} // PROJECTING ARRAYS INTO BaseArray
+	// PULLING FILES LOGIC
+
+	max := users.eprt
+	for i < N-1 {
+		if users.eprt > max {
+			max = users.eprt
+		}
+		i++
+	}
+
+	return int(max)
 }
 
 func lowestGpa() {
