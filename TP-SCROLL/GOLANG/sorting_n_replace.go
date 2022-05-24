d059b1b6e44417b0fd07a280ea84905daa8c8b69
Ikhsan Assidiqie <91542488+ikhsansdqq@users.noreply.github.com>
2022-05-24 15:23:03 +0700

    Sorting and Replace Algorithm

---
+++ TP-SCROLL/GOLANG/sorting_n_replace.go
@@ -0,0 +1,27 @@
+package main
+
+func main() {
+	var (
+		arrayCustom = [10]int{
+			20,
+			10,
+			40,
+			30,
+			70,
+			80,
+			50,
+			60,
+			100,
+			90,
+		}
+	)
+
+	//	j < i
+	//		temp = i
+	//		i = j
+	//		j = temp
+	//	[20,< 10, 40, 30, 70, 80, 50, 60, 100, 90]
+
+	//fmt.Println(arrayCustom[9]) => max point arrayCustom index: 9
+	//	positioning harus ada i sama j
+}
