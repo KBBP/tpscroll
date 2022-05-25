ccc0d4d086488b2c7d8332e06d10b0305afe9470
Ikhsan Assidiqie <91542488+ikhsansdqq@users.noreply.github.com>
2022-05-25 07:18:16 +0700

    main

--- TP-SCROLL/GOLANG/TP_ALPRO_MOD11_IF45INT_1301213047/competition.go
+++ TP-SCROLL/GOLANG/TP_ALPRO_MOD11_IF45INT_1301213047/competition.go
@@ -1,25 +1,33 @@
 package main
 
-import "fmt"
-
-//func sort(tabReceiver []int) {
-//	const temp = 0
-//
-//	for i := 0; i < len(tabReceiver); i++ {
-//		fmt.Println(tabReceiver[i])
-//	}
-//}
+import (
+	"fmt"
+)
 
 func main() {
-	var inputNumber int
-	var storeArray []int
-	fmt.Scanln(&inputNumber)
-	storeArray[0] = inputNumber
+	var sizeArray int
 
-	for i := 1; i < len(storeArray); i++ {
-		storeArray[i] = inputNumber
-		fmt.Scanln(&inputNumber)
+	fmt.Printf("Enter size of your array: ")
+	fmt.Scanln(&sizeArray)
+
+	var myArr = make([]int, sizeArray)
+
+	for i := 0; i < sizeArray; i++ {
+		fmt.Printf("Enter %dth element: ", i)
+		fmt.Scan(&myArr[i])
 	}
 
-	//sort(storeArray)
+	// Finding Median Function
+
+	for i := 0; i < sizeArray; i++ {
+		for x := 1; x < len(myArr); i++ {
+
+		}
+	}
+
+	// Array Template: [6, 3, 8, 2](4) = sizeOf 4
+
+	fmt.Println("Your array is: ", myArr)
+	fmt.Println("With length of: ", len(myArr))
+
 }
